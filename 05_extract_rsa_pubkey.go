41754418c60c1dc60cbb0817edeedf6a5e372474
Eleanor McHugh <eleanor@games-with-brains.com>
2019-10-02 19:38:09 +0100

    how to read a PEM encoded private key and produce a PEM encoded public key from it

---
+++ 05_extract_rsa_pubkey.go
@@ -0,0 +1,84 @@
+
+package main
+
+import "crypto/rsa"
+import "crypto/x509"
+import "encoding/pem"
+import "io/ioutil"
+import "os"
+
+const (
+  MISSING_FILENAME = 1
+  FILE_UNREADABLE = iota
+  NOT_A_PEM_FILE = iota
+  NOT_A_PRIVATE_KEY = iota
+  PEM_PASSWORD_REQUIRED = iota
+  PEM_DECRYPTION_FAILED = iota
+  INVALID_PRIVATE_KEY = iota
+  FILE_WRITE_FAILED = iota
+)
+
+func main() {
+  var e error
+  var k *rsa.PrivateKey
+
+  f := os.Args[1]
+  b := LoadFile(f)
+  p := DecodePEM(b)
+  b = DecryptPEM(p)
+
+  if k, e = x509.ParsePKCS1PrivateKey(b); e != nil {
+    os.Exit(INVALID_PRIVATE_KEY)
+  }
+
+  p = CreatePEM(k.PublicKey)
+  f = f + ".pub"
+  if e = ioutil.WriteFile(f, pem.EncodeToMemory(p), 0644); e != nil {
+    os.Exit(FILE_WRITE_FAILED)
+  }
+}
+
+func LoadFile(s string) (b []byte) {
+  var e error
+  if s == "" {
+    os.Exit(MISSING_FILENAME)
+  }
+
+  if b, e = ioutil.ReadFile(s); e != nil {
+    os.Exit(FILE_UNREADABLE)
+  }
+  return
+}
+
+func CreatePEM(k rsa.PublicKey) *pem.Block {
+  return &pem.Block{
+    Type:  "RSA PUBLIC KEY",
+    Bytes: x509.MarshalPKCS1PublicKey(&k),
+  }
+}
+
+func DecodePEM(b []byte) (p *pem.Block) {
+  switch p, _ = pem.Decode(b); {
+  case p == nil:
+    os.Exit(NOT_A_PEM_FILE)
+  case p.Type != "RSA PRIVATE KEY":
+    os.Exit(NOT_A_PRIVATE_KEY)
+  }
+  return
+}
+
+func DecryptPEM(p *pem.Block) (b []byte) {
+  if x509.IsEncryptedPEMBlock(p) {
+	if pwd := os.Getenv("PEM_KEY"); pwd != "" {
+      var e error
+      if b, e = x509.DecryptPEMBlock(p, []byte(pwd)); e != nil {
+        os.Exit(PEM_DECRYPTION_FAILED)
+      }
+    } else {
+      os.Exit(PEM_PASSWORD_REQUIRED)
+    }
+  } else {
+    b = p.Bytes
+  }
+  return
+}
