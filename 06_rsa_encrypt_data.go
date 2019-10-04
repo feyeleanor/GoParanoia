c84a18bb78cd9c5818710c1da3aa2c68e2f9c822
Eleanor McHugh <eleanor@games-with-brains.com>
2019-10-04 15:33:47 +0100

    encrypting and decrypting with RSA using PKCS1

---
+++ 06_rsa_encrypt_data.go
@@ -0,0 +1,80 @@
+
+package main
+
+import "crypto/rand"
+import "crypto/rsa"
+import "crypto/x509"
+import "encoding/pem"
+import "fmt"
+import "io/ioutil"
+import "os"
+
+const (
+  MISSING_FILENAME = 1
+  FILE_UNREADABLE = iota
+  NOT_A_PEM_FILE = iota
+  NOT_A_PUBLIC_KEY = iota
+  PEM_PASSWORD_REQUIRED = iota
+  PEM_DECRYPTION_FAILED = iota
+  INVALID_PUBLIC_KEY = iota
+  ENCRYPTION_FAILED = iota
+)
+
+func main() {
+  var e error
+  var k *rsa.PublicKey
+
+  f := os.Args[1]
+  m := []byte(os.Args[2])
+  b := LoadFile(f)
+  p := DecodePEM(b)
+  b = DecryptPEM(p)
+
+  if k, e = x509.ParsePKCS1PublicKey(b); e != nil {
+    os.Exit(INVALID_PUBLIC_KEY)
+  }
+
+  if b, e = rsa.EncryptPKCS1v15(rand.Reader, k, m); e != nil {
+    fmt.Println(e)
+    os.Exit(ENCRYPTION_FAILED)
+  }
+  fmt.Println(b)
+}
+
+func LoadFile(s string) (b []byte) {
+  var e error
+  if s == "" {
+    os.Exit(MISSING_FILENAME)
+  }
+
+  if b, e = ioutil.ReadFile(s); e != nil {
+    os.Exit(FILE_UNREADABLE)
+  }
+  return
+}
+
+func DecodePEM(b []byte) (p *pem.Block) {
+  switch p, _ = pem.Decode(b); {
+  case p == nil:
+    os.Exit(NOT_A_PEM_FILE)
+  case p.Type != "RSA PUBLIC KEY":
+    os.Exit(NOT_A_PUBLIC_KEY)
+  }
+  return
+}
+
+func DecryptPEM(p *pem.Block) (b []byte) {
+  if x509.IsEncryptedPEMBlock(p) {
+	if pwd := os.Getenv("PEM_KEY"); pwd != "" {
+      var e error
+      if b, e = x509.DecryptPEMBlock(p, []byte(pwd)); e != nil {
+        os.Exit(PEM_DECRYPTION_FAILED)
+      }
+    } else {
+      os.Exit(PEM_PASSWORD_REQUIRED)
+    }
+  } else {
+    b = p.Bytes
+  }
+  return
+}
---
+++ 07_rsa_decrypt_data.go
@@ -0,0 +1,89 @@
+
+package main
+
+import "crypto/rand"
+import "crypto/rsa"
+import "crypto/x509"
+import "encoding/pem"
+import "fmt"
+import "io/ioutil"
+import "os"
+import "strconv"
+
+const (
+  MISSING_FILENAME = 1
+  FILE_UNREADABLE = iota
+  NOT_A_PEM_FILE = iota
+  NOT_A_PRIVATE_KEY = iota
+  PEM_PASSWORD_REQUIRED = iota
+  PEM_DECRYPTION_FAILED = iota
+  INVALID_PRIVATE_KEY = iota
+  DECRYPTION_FAILED = iota
+)
+
+func main() {
+  var e error
+  var k *rsa.PrivateKey
+
+  f := os.Args[1]
+  m := read_bytes(os.Args[2:])
+  b := LoadFile(f)
+  p := DecodePEM(b)
+  b = DecryptPEM(p)
+
+  if k, e = x509.ParsePKCS1PrivateKey(b); e != nil {
+    os.Exit(INVALID_PRIVATE_KEY)
+  }
+
+  if b, e = rsa.DecryptPKCS1v15(rand.Reader, k, m); e != nil {
+    fmt.Println(e)
+    os.Exit(DECRYPTION_FAILED)
+  }
+  fmt.Println(string(b))
+}
+
+func read_bytes(s []string) (r []byte) {
+  for _, v := range s {
+    i, _ := strconv.Atoi(v)
+    r = append(r, byte(i))
+  }
+  return
+}
+
+func LoadFile(s string) (b []byte) {
+  var e error
+  if s == "" {
+    os.Exit(MISSING_FILENAME)
+  }
+
+  if b, e = ioutil.ReadFile(s); e != nil {
+    os.Exit(FILE_UNREADABLE)
+  }
+  return
+}
+
+func DecodePEM(b []byte) (p *pem.Block) {
+  switch p, _ = pem.Decode(b); {
+  case p == nil:
+    os.Exit(NOT_A_PEM_FILE)
+  case p.Type != "RSA PRIVATE KEY":
+    os.Exit(NOT_A_PRIVATE_KEY)
+  }
+  return
+}
+
+func DecryptPEM(p *pem.Block) (b []byte) {
+  if x509.IsEncryptedPEMBlock(p) {
+	if pwd := os.Getenv("PEM_KEY"); pwd != "" {
+      var e error
+      if b, e = x509.DecryptPEMBlock(p, []byte(pwd)); e != nil {
+        os.Exit(PEM_DECRYPTION_FAILED)
+      }
+    } else {
+      os.Exit(PEM_PASSWORD_REQUIRED)
+    }
+  } else {
+    b = p.Bytes
+  }
+  return
+}
